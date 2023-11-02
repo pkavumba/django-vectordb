a439a7ef249ae90bb48a93657862181d285f0ec9
pkavumba <pkavumba@gmail.com>
2023-11-02 19:03:09 +0900

    refactor

--- vectordb/embedding_functions.py
+++ vectordb/embedding_functions.py
@@ -17,13 +17,13 @@ except ImportError:
 class SentenceTransformerEncoder:
     _instances: dict[str, SentenceTransformerEncoder] = {}
 
-    def __new__(cls, model_name="all-MiniLM-L6-v2", *args, **kwargs):
+    def __new__(cls, model_name: str = "all-MiniLM-L6-v2", *args, **kwargs):
         if model_name not in cls._instances:
             instance = super().__new__(cls)
             cls._instances[model_name] = instance
         return cls._instances[model_name]
 
-    def __init__(self, model_name="all-MiniLM-L6-v2"):
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
         if SentenceTransformer is None:
             raise ImportError(
                 "SentenceTransformer is not installed. Please install sentence-transformers package."  # noqa
@@ -32,7 +32,7 @@ class SentenceTransformerEncoder:
         if not hasattr(self, "model"):
             self.model = SentenceTransformer(model_name)
 
-    def __call__(self, texts: list[str]):
+    def __call__(self, texts: list[str]) -> np.ndarray:
         return self.model.encode(texts, convert_to_numpy=True)
 
 
@@ -47,13 +47,13 @@ class OpenAIEmbeddings:
         openai.api_key = settings.OPENAI_API_KEY
         self.model = model_name
 
-    def get_embedding(self, text):
+    def get_embedding(self, text: str) -> np.ndarray:
         text = text.replace("\n", " ")
         response = openai.Embedding.create(input=[text], model=self.model)
         raw_embedding = response["data"][0]["embedding"]
         return np.array(raw_embedding, dtype=np.float32)
 
-    def __call__(self, text):
-        if type(text) is list:
+    def __call__(self, text: str | list[str]) -> np.ndarray:
+        if isinstance()(text, list):
             return [self.get_embedding(t) for t in text]
         return self.get_embedding(text)
